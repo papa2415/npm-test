b9e87bcd5336e3b7e75815b4755ba6ecfda884e0
papa2415 <167390811+papa2415@users.noreply.github.com>
2024-08-08 14:18:22 +0800

    update-1

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,18 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from 'vue'
+const num = ref(15) //定義數字
+function clickMe() {
+  num.value++ //存入的值會存在 .value鍾
+}
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    {{ num }}
+    <!-- 用兩個{{  }}  渲染出來-->
+    <button type="button" v-on:click="clickMe">點我</button>
+    <!-- v-on監聽 點擊事件-->
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
+<style scoped></style>
